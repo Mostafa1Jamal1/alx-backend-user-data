4d0ee1a4bcf854bfa6fc8a3376b0a3c21c0727a3
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-09-04 04:32:49 +0300

    3. Auth class in 0x01-Basic_authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Auth module
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """Auth class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """returns False - path
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """returns None
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """returns None
+        """
+        return None
