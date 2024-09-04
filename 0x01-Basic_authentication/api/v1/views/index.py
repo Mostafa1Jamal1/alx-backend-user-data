5cd12ea7d0a176aa63a6534a6aeede5d286fa297
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-09-04 04:15:02 +0300

    2. Error handler: Forbidden in 0x01-Basic_authentication

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -34,3 +34,13 @@ def unauthorized() -> str:
     """
     abort(401)
     return
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - raise a 403 error
+    """
+    abort(403)
+    return
