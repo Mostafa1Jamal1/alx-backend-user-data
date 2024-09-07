e23a0aa855b03233b44f6598064b425e8e37d636
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-09-07 16:55:32 +0300

    8. Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 """
 
 from .auth import Auth
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
@@ -16,4 +17,18 @@ class BasicAuth(Auth):
                 type(authorization_header) == str and
                 authorization_header[:6] == 'Basic '):
             return authorization_header[6:]
+        return None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """decode value of a Base64 string base64_authorization_header
+        """
+        if (base64_authorization_header is not None and
+                type(base64_authorization_header) == str):
+            try:
+                binary_like = base64_authorization_header.encode('utf-8')
+                decoded64 = b64decode(binary_like)
+                return decoded64.decode('utf-8')
+            except Exception:
+                return None
         return None
