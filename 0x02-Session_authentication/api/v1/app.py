878f0294706b56b18eaa1aa0bebb33294517145f
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-09-08 01:13:57 +0300

    5. Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -33,10 +33,13 @@ def auth_handler():
             request.path,
             ['/api/v1/status/',
              '/api/v1/unauthorized/',
-             '/api/v1/forbidden/'])):
+             '/api/v1/forbidden/',
+             '/api/v1/auth_session/login/'])):
             return
-        if auth.authorization_header(request) is None:
+        if (auth.authorization_header(request) is None and
+                auth.session_cookie(request) is None):
             abort(401)
+
         if auth.current_user(request) is None:
             abort(403)
         request.current_user = auth.current_user(request)
