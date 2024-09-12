84e526ca839625f4423b36fdbbf780258e64807e
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-09-13 00:30:06 +0300

    fix: function name

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -74,5 +74,5 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """updates the corresponding user’s session ID to None.
         """
-        user = self.get_user_from_session_id(id=user_id)
+        user = self.find_user_by(id=user_id)
         user.session_id = None
