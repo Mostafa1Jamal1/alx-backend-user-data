7a80b29e5ed58c300ccf89e3058511e6b7c995af
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-09-12 22:29:17 +0300

    9. Generate UUIDs

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -15,6 +15,11 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
+def _generate_uuid() -> str:
+    """return a string representation of a new UUID
+    """
+    return str(uuid.uuid4())
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -43,8 +48,3 @@ class Auth:
                 password.encode('utf-8'), user.hashed_password)
         except NoResultFound:
             return False
-
-    def _generate_uuid(self) -> str:
-        """return a string representation of a new UUID
-        """
-        return str(uuid.uuid4())
