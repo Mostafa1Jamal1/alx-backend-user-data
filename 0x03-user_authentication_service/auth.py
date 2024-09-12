fa71e5e4b5aab1bf23ad8f68494f0e71a140a383
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-09-12 21:59:31 +0300

    8. Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -32,3 +32,13 @@ class Auth:
             user = self._db.add_user(
                 email=email, hashed_password=_hash_password(password))
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Credentials validation
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(
+                password.encode('utf-8'), user.hashed_password)
+        except NoResultFound:
+            return False
