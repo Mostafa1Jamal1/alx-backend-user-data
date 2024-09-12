38eef29095a94d4efca28cde5e410b3b71604617
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-09-12 17:34:38 +0300

    2. Find user in 0x03-user_authentication_service

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
 """DB module
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from typing import TypeVar
 
 from user import Base, User
@@ -37,3 +40,15 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> TypeVar('User'):
+        """returns the first row found in the users table
+        as filtered by the method’s input arguments
+        """
+        user = (self._session
+                .query(User)
+                .filter_by(**kwargs)
+                .first())
+        if not user:
+            raise NoResultFound
+        return user
